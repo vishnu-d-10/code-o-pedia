1b10a180a4edcf5ee3ac9f310ff61c7595fb9b8e
vishnu-d-10 <vishnudsr1999@gmail.com>
2022-02-15 22:53:49 +0530

    added changes after testing

--- Projects/Weather_Info/main.py
+++ Projects/Weather_Info/main.py
@@ -1,12 +1,11 @@
-!pip3 install requests
+import requests # A Python lib to send requests
+from pprint import pprint
 
-import requests
-
-api_key = "AAAAPPPPIIII_____KKKKEEEEYYYY"  # Enter the API key you got from the OpenWeatherMap website
+api_key = "2dc6bbe4fe2be84d8b9668295e9f0fc9"  # Enter the API key you got from the OpenWeatherMap website
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 city_name = input("Enter city name : ")
-complete_url = base_url + "appid=" + 'd850f7f52bf19300a9eb4b0aa6b80f0d' + "&q=" + city_name  # This is to complete the base_url, you can also do this manually to checkout other weather data available
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name  # This is to complete the base_url, you can also do this manually to checkout other weather data available
 response = requests.get(complete_url)
 x = response.json()
 
@@ -20,7 +19,7 @@ if x["cod"] != "404":
 
     print(" Temperature (in kelvin unit) = " +
                     str(current_temperature) +
-          "\n description = " +
+          "\n Description = " +
                     str(weather_description))
 
 else:
